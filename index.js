2cc41834df9a41e634af5889bf6eda6e5f73d34b
iit2015143 <iit2015143@iiita.ac.in>
2018-02-17 00:51:09 +0530

    Updated navigation animation for pc only

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+clicked = 0;
+navshifted = 0;
 window.onload = function(){
     $("#downbutton").show();
     $("#downbutton").click(function(){
@@ -6,6 +8,45 @@ window.onload = function(){
         $("body").css({
             overflow:'auto'
         });
+    });
+    $(window).scroll(function(){
+        if(clicked == 0){
+            $("nav").slideDown();
+        }
+        else {
+            $("nav").slideUp();
+            clicked = 0;
+        }
+        //console.log($("header").outerHeight()-$(window).scrollTop());
+        if($("header").outerHeight() - $(window).scrollTop() <= 0  && navshifted == 0){
+            navheight = $("nav").height()+5;
+            navheight += ($("article").outerHeight(true) - $("article").outerHeight())/2;
+            navheight += 'px';
+            console.log(navheight);
+            $("nav").css({
+                position:'fixed',
+                top:'0px',
+                left:'0px',
+                width:'100%',
+                zIndex:'100'
+            });
+            $("header").css({
+                marginBottom:navheight
+            });
+            navshifted = 1;
+        }
+        else if($("header").outerHeight() - $(window).scrollTop() > 0  && navshifted == 1){
+            $("nav").css({
+                position:'static'
+            });
+            $("header").css({
+                marginBottom:'0px',
+            });
+            navshifted = 0;
+        }
+    });
+    $("nav ul li").click(function(event){
+        clicked = 1;
     });
 };
 function initMap() {
